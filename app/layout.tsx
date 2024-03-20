4b230817cf2b47744fe5620dcc545e587cea0fb1
Ayush Mathpal <ayush@192.168.1.2>
2024-03-20 14:04:08 +0530

    Push project for production

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: 'Evendo',
+  description: 'Evendo is a one stop solution to promoting your event.',
+  icons: {
+    icon: '/assets/images/logo1.png'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
