5e878677140afe4e9fc20bb61d71b34302c1f87b
Ayush Mathpal <ayush@192.168.1.2>
2024-03-21 21:25:26 +0530

    Implement Stripe

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -13,14 +13,14 @@ import User from '../database/models/user.model';
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-  const price = order.isFree ? 0 : Number(order.price) * 100;
+  const price = order.isFree ? 0 : Number(order.price) * 100
 
   try {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: 'usd',
+            currency: 'inr',
             unit_amount: price,
             product_data: {
               name: order.eventTitle
